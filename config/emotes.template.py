73f51b256118e51b2755c7a6c1d59a36ebcf3687
sinus-x <14353790+sinus-x@users.noreply.github.com>
2020-03-29 03:00:17 +0200

    Add emotes template

---
+++ config/emotes.template.py
@@ -0,0 +1,17 @@
+class Emotes:
+	happy =  '<:code:000'
+	love =   '<:code:000>'
+	sad =    '<:code:000>'
+	angry =  '<:code:000>'
+	ree =    '<:code:000>'
+	scared = '<:code:000>'
+	wtf =    '<:code:000>'
+	ok =     '<:code:000>'
+
+	panic =     '<:code:000>'
+	facepalm =  '<:code:000>'
+	wave =      '<:code:000>'
+	hug_left =  '<:code:000>'
+	hug_right = '<:code:000>'
+	objection = '<:code:000>'
+	tagrage =   '<:code:000>'
