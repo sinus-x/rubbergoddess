c948e858ecbfcd65ab500db999ff2dc0723a7f30
sinus-x <14353790+sinus-x@users.noreply.github.com>
2020-03-29 23:13:45 +0200

    Verify reworked
    Now you can't feel pain just by looking at it

--- config/messages.py
+++ config/messages.py
@@ -97,31 +97,39 @@ class Messages:
     rd_help = "Formát naleznete na https://wiki.roll20.net/Dice_Reference\n" \
               "Implementovány featury podle obsahu: **8. Drop/Keep**"
 
-    verify_already_verified = "{user} Už jste byli ověřeni ({admin} pls)."
-    verify_send_format = "Čekám jeden argument. Správný formát:\n" \
-                           "`{}submit identifikátor kód`".format(prefix)
-    verify_send_dumbshit = "{user} Tvůj login. {emote}"
-    verify_send_success = "{user} Kód byl odeslán na tvůj mail ({mail})!\n" + \
-                          "Pro verifikaci použij: `" + prefix + "submit identifikátor kód`"
-    verify_send_not_found = "{user} Login nenalezen nebo jste tímto krokem již prošli ({admin} pls)."
-    verify_verify_format = "Očekávám tři argumenty. Správný formát:\n" + \
-                           "`{}submit xlogin00`\n\n".format(prefix) + \
-                           "Pro získání kódu použij příkaz podle příslušnosti:\n" + \
-                           "`{}verify FEKT xlogin00`\n".format(prefix) + \
-                           "`{}verify VUT  xlogin00`\n".format(prefix) + \
-                           "`{}verify MUNI UČO`\n".format(prefix) + \
-                           "`{}verify jine host`".format(prefix)
-    verify_verify_dumbshit = "{user} Kód, který ti přišel na mail. {emote}"
-    verify_verify_manual = "{user}, nechám {admin}, aby to udělal manuálně ({year})"
+    # VERIFY
+    verify_no_email = "__Tvůj__ e-mail, {user} {emote}"
+    verify_no_login = "__Tvůj__ login, {user} {emote}"
+
+    verify_send_dumbshit = "{user}, použij __svůj__ identifikátor. " + emote.facepalm
+    verify_already_sent = "{user}, e-mail už jsem ti jednou poslala ({admin}...?)"
+    verify_already_verified = "Tebe už jsem si prověřila, {user}..."
+    verify_send_kicked = "{admin}, {user} byl vykopnut a snaží se verifikovat."
+    verify_send_banned = "{admin}, {user} byl zabanován a snaží se verifikovat."
+    verify_send_success = "{user} Na e-mail ({mail}) jsem ti poslala ověřovací kód.\n" + \
+                          "Pro verifikaci použij: `" + prefix + "submit {id} kód`"
+    verify_send_error = "{user}, došlo k chybě " + emote.sad + ". {admin}?"
+    verify_send_format = "{user}:\n" \
+        "```" + prefix + "verify [FEKT|VUT] xlogin00\n" + \
+        prefix + "verify email```"
+
+    verify_verify_no_code = "{user} Ten kód, který jsem ti poslala na e-mail {emote}"
+    verify_verify_not_found = "{user}, takový login jsem nenašla."
+    verify_verify_wrong_code = "{user} Špatný kód."
+    verify_verify_manual = "{admin}, {user} asi chybí skupina ({year})."
     verify_verify_success = "{user} Gratuluji k verifikaci!"
-    verify_post_verify_fekt = "Obor si zapiš v <#692086702382121010>\n\n" \
+    verify_verify_success_info = "Nový uživatel {user} byl úspěšně přidán s rolí {group}."
+    verify_congrats_fekt = "Obor si zapiš v <#692086702382121010>\n\n" \
                               "V <#692084608778633217> získáš další role pro zájmy\n" \
                               "Obecné informace jsou v <#692084651849678938>."
-    verify_post_verify_guest = "V <#692084608778633217> získáš role pro zájmy\n" \
+    verify_congrats_guest = "V <#692084608778633217> získáš role pro zájmy\n" \
                               "Obecné informace jsou v <#692084651849678938>."
 
-    verify_verify_not_found = "{user} Login nenalezen nebo jste tímto krokem již prošli ({admin} pls)."
-    verify_verify_wrong_code = "{user} Špatný kód."
+    verify_verify_format = "Pro získání kódu použij příkaz podle příslušnosti:```\n" + \
+                           "{}verify FEKT xlogin00\n".format(prefix) + \
+                           "{}verify VUT  xlogin00\n".format(prefix) + \
+                           "{}verify e-mail```\n".format(prefix) + \
+                           "xlogin00 nebo e-mail je váš identifikátor pro další krok."
 
     vote_format = "Použití vote:\n```" \
         + prefix + "vote [datum] [čas] [otázka]\n" \
--- repository/user_repo.py
+++ repository/user_repo.py
@@ -2,42 +2,46 @@ from repository.base_repository import BaseRepository
 from repository.database import session
 from repository.database.verification import Permit, Valid_person
 
-from pprint import pprint
-
 class UserRepository(BaseRepository):
-    # Status 0 = verified
-    # Status 1 = unverified
-    # Status 2 = in process of verification (code sent)
+    # 0 ... unknown
+    # 1 ... pending
+    # 2 ... verified
+    # 3 ... kicked
+    # 4 ... banned
 
     def save_sent_code(self, login: str, code: str):
-        """Updates a specified login with a new verification code"""
+        """Update a specified login with a new verification code"""
         person = session.query(Valid_person).filter(Valid_person.login == login).one_or_none()
         person.code = code
-        person.status = 2
+        person.status = 1
         session.commit()
 
     def save_verified(self, login: str, discord_id: str):
-        """"Inserts login with discord name into database"""
+        """Insert login with discord name into database"""
         session.add(Permit(login=login, discord_ID=discord_id))
-
         person = session.query(Valid_person).filter(Valid_person.login == login).one_or_none()
-        person.status = 0
-
+        person.status = 2
         session.commit()
 
     def has_unverified_login(self, login: str):
-        """"Checks if there's a login """
+        """Check if there's a login """
+        #TODO check twice for FEKT/VUT option
         query = session.query(Valid_person).filter(
             Valid_person.login == login, Valid_person.status == 1).one_or_none()
         return True if query is not None else False
 
-    def get_user(self, login: str, status: int = 2):
-        """"Finds login from database"""
-        user = session.query(Valid_person).filter(
-            Valid_person.login == login, Valid_person.status == status).one_or_none()
+    def get_user(self, login: str, status):
+        """Find login from database"""
+        #TODO check twice for FEKT/VUT option
+        if not status:
+            user = session.query(Valid_person).filter(Valid_person.login == login).one_or_none()
+        else:
+            user = session.query(Valid_person).filter(
+                Valid_person.login == login, Valid_person.status == status).one_or_none()
         return user
 
-    def add_user(self, login: str, almamater: str, status: int = 1):
-        """"Finds login from database"""
-        session.add(Valid_person(login=login, year=almamater, status=status))
+    def add_user(self, login: str, group: str, status: int = 1):
+        #TODO check twice for FEKT/VUT option
+        """Find login from database"""
+        session.add(Valid_person(login=login, year=group, status=status))
         session.commit()
