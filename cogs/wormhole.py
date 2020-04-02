24bcd61a3f551a80029297990b2688860f83d163
sinus-x <14353790+sinus-x@users.noreply.github.com>
2020-04-02 14:37:33 +0200

    Wormhole patch
    get_user >> fetch_user

--- cogs/wormhole.py
+++ cogs/wormhole.py
@@ -1,17 +1,23 @@
 import discord
 from discord.ext import commands
 
-from config.config import Config as config
+from config import config
+
+config = config.Config
+
 
 class Wormhole(commands.Cog):
-    """A wormhole for crossposting between two servers"""
     def __init__(self, bot):
         self.bot = bot
-        self.sibling = self.bot.get_user(config.wormhole_sibling_id)
+        self.sibling = None
+        self.sibling_id = config.wormhole_sibling_id
         self.wormhole = self.bot.get_channel(config.wormhole_channel_id)
 
     @commands.Cog.listener()
     async def on_message(self, message):
+        if self.sibling is None:
+            self.sibling = await self.bot.fetch_user(self.sibling_id)
+
         if message.channel == self.wormhole and not message.author.bot:
             self.sibling.send(discord.utils.escape_mentions(message.content))
         elif (isinstance(message.channel, discord.DMChannel) and
