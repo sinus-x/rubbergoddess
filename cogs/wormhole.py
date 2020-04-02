d85c7cea345c16feba65b8e4fb697bc5596dd5b3
rubbergoddess <rubbergoddess@email.cz>
2020-04-02 16:21:53 +0200

    Working wormhole implementation

--- cogs/wormhole.py
+++ cogs/wormhole.py
@@ -1,28 +1,25 @@
 import discord
 from discord.ext import commands
 
-from config import config
+from config.config import Config as config
 
-config = config.Config
-
-
-class Wormhole(commands.Cog):
+class Wormhole (commands.Cog):
+    """Allow sending messages between two servers"""
     def __init__(self, bot):
         self.bot = bot
-        self.sibling = None
-        self.sibling_id = config.wormhole_sibling_id
-        self.wormhole = self.bot.get_channel(config.wormhole_channel_id)
+        self.warp_orange = self.bot.get_channel(config.wormhole_source_channel)
+        self.warp_blue = self.bot.get_channel(config.wormhole_target_channel)
 
     @commands.Cog.listener()
-    async def on_message(self, message):
-        if self.sibling is None:
-            self.sibling = await self.bot.fetch_user(self.sibling_id)
+    async def on_message (self, message):
+        if message.author == self.bot.user:
+            return
 
-        if message.channel == self.wormhole and not message.author.bot:
-            self.sibling.send(discord.utils.escape_mentions(message.content))
-        elif (isinstance(message.channel, discord.DMChannel) and
-              message.channel.recipient == self.sibling):
-            self.wormhole.send(message.content)
+        if message.channel == self.warp_blue:
+            await self.warp_orange.send(discord.utils.escape_mentions(message.content))
+        elif message.channel == self.warp_orange:
+            await self.warp_blue.send(discord.utils.escape_mentions("> " + message.content))
 
 def setup(bot):
     bot.add_cog(Wormhole(bot))
+
