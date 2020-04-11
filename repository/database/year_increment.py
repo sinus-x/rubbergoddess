01809c08fa39c1696c63826ea7abe2542bc12451
sinus-x <14353790+sinus-x@users.noreply.github.com>
2020-04-12 01:02:47 +0200

    Remove unused FITWIDE files

--- repository/database/year_increment.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, String
-from repository.database import database
-
-
-class User_backup(database.base):
-    __tablename__ = 'bot_user_backup'
-
-    member_ID = Column(String, primary_key=True)
