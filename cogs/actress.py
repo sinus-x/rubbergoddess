ddd9b7ed2999cd13bd6ed435ef23114176ebca28
sinus-x <14353790+sinus-x@users.noreply.github.com>
2020-06-17 14:19:00 +0200

    Actress: Scrolling

--- cogs/actress.py
+++ cogs/actress.py
@@ -1,7 +1,6 @@
 import hjson
 import os
 import shlex
-import time
 from requests import get
 
 import discord
@@ -69,7 +68,7 @@ class Actress(rubbercog.Rubbercog):
         try:
             name = next(iter(self.reactions))
             reaction = self.reactions[name]
-            embed = self.embed(ctx=ctx, description=f"**{name}**", page=(1, len(self.reactions)))
+            embed = self.embed(ctx=ctx, page=(1, len(self.reactions)))
         except StopIteration:
             reaction = None
             embed = self.embed(ctx=ctx)
@@ -87,6 +86,7 @@ class Actress(rubbercog.Rubbercog):
     async def react_add(self, ctx, name: str = None, *, parameters=None):
         """Add new reaction
 
+        ```
         ?react add <reaction name>
         type <image | text>
         match <full | start | end | any>
@@ -96,14 +96,14 @@ class Actress(rubbercog.Rubbercog):
 
         users 0 1 2
         channels 0 1 2
-        counter <int>
+        counter 10
+        ```
         """
         if name is None:
             return await utils.send_help(ctx)
         elif name in self.reactions.keys():
             raise ReactionNameExists()
 
-        # TODO This may not save all the key-value pairs
         reaction = await self.parse_react_message(ctx.message, strict=True)
         self.reactions[name] = reaction
         self._save_reactions()
@@ -113,7 +113,21 @@ class Actress(rubbercog.Rubbercog):
 
     @react.command(name="edit")
     async def react_edit(self, ctx, name: str = None, *, parameters=None):
-        """Edit reaction"""
+        """Edit reaction
+
+        ```
+        ?react edit <reaction name>
+        type <image | text>
+        match <full | start | end | any>
+        sensitive <true | false>
+        triggers "a b c" "d e" f
+        responses "abc def"
+
+        users 0 1 2
+        channels 0 1 2
+        counter 10
+        ```
+        """
         if name is None:
             return await utils.send_help(ctx)
         elif name not in self.reactions.keys():
@@ -137,7 +151,7 @@ class Actress(rubbercog.Rubbercog):
         """Remove reaction"""
         if name is None:
             return await utils.send_help(ctx)
-        elif name not in self.reaction.keys():
+        elif name not in self.reactions.keys():
             raise ReactionNotFound()
 
         del self.reactions[name]
@@ -174,20 +188,19 @@ class Actress(rubbercog.Rubbercog):
             return  # TODO Remove reaction
 
         # get page
-        footer = embed.footer.text.split(" | ")[-1]
-        page_current = int(footer.split("/")[0])
-        pages_total = int(footer.split("/")[1])
+        footer_text = embed.footer.text
+        pages = footer_text.split(" | ")[-1]
+        page_current = int(pages.split("/")[0]) - 1
 
         page = (page_current + page_delta) % len(self.reactions)
+        footer_text = footer_text.replace(pages, f"{page+1}/{len(self.reactions)}")
 
         # update embed
-        bot_reaction_name = list(self.reactions.keys())[page + 1]
-        print(bot_reaction_name)
+        bot_reaction_name = list(self.reactions.keys())[page]
         bot_reaction = self.reactions[bot_reaction_name]
 
-        print(f"New page is {page}")
-
         embed = self.fill_reaction_embed(embed, bot_reaction)
+        embed.set_footer(text=footer_text, icon_url=embed.footer.icon_url)
         await reaction.message.edit(embed=embed)
 
         # remove reaction
@@ -216,6 +229,18 @@ class Actress(rubbercog.Rubbercog):
             key = line[0]
             value = line[1]
 
+            if key not in (
+                "type",
+                "match",
+                "sensitive",
+                "triggers",
+                "responses",
+                "users",
+                "channels",
+                "counter",
+            ):
+                raise InvalidReactionKey(key=key)
+
             # check
             invalid = False
             # fmt: off
@@ -225,9 +250,6 @@ class Actress(rubbercog.Rubbercog):
             or key == "triggers" and len(value) < 1 \
             or key == "responses" and len(value) < 1:
                 invalid = True
-            elif key not in ("users", "channels", "counter"):
-                # unsupported key
-                continue
 
             # fmt: on
             if invalid:
@@ -265,25 +287,23 @@ class Actress(rubbercog.Rubbercog):
         # reset any previous
         embed.clear_fields()
 
-        print(reaction)
-
-        for key in ("type", "match", "sensitive"):
-            embed.add_field(name=key, value=reaction[key])
         for key in ("triggers", "responses"):
             value = "\n".join(reaction[key])
             embed.add_field(name=key, value=value, inline=False)
-        if reaction["users"] is not None:
+        for key in ("type", "match", "sensitive"):
+            embed.add_field(name=key, value=reaction[key])
+        if "users" in reaction.keys() and reaction["users"] is not None:
             users = [self.bot.get_user(x) for x in reaction["users"]]
             value = "\n".join(
                 f"`{user.id}` {user.name if hasattr(user, 'name') else '_unknown_'}"
                 for user in users
             )
             embed.add_field(name="users", value=value, inline=False)
-        if reaction["channels"] is not None:
+        if "channels" in reaction.keys() and reaction["channels"] is not None:
             channels = [self.bot.get_channel(x) for x in reaction["channels"]]
             value = "\n".join(f"`{channel.id}` {channel.mention}" for channel in channels)
             embed.add_field(name="channels", value=value, inline=False)
-        if reaction["counter"] is not None:
+        if "counter" in reaction.keys() and reaction["counter"] is not None:
             embed.add_field(name="countdown", value=str(reaction["counter"]))
 
         return embed
@@ -304,7 +324,10 @@ class Actress(rubbercog.Rubbercog):
 
         # fmt: off
         # exceptions with parameters
-        if isinstance(error, ReactionParsingException):
+        if isinstance(error, InvalidReactionKey):
+            await self.output.error(ctx, text.fill(
+                "actress", "InvalidReactionKey", key=error.key))
+        elif isinstance(error, ReactionParsingException):
             await self.output.error(ctx, text.fill(
                 "actress", "ReactionParsingException", key=error.key, value=error.value))
         # exceptions without parameters
@@ -331,6 +354,12 @@ class ReactionNameExists(ReactionException):
 
 class ReactionNotFound(ReactionException):
     pass
+
+
+class InvalidReactionKey(ReactionException):
+    def __init__(self, key: str):
+        super().__init__()
+        self.key = key
 
 
 class ReactionParsingException(ReactionException):
