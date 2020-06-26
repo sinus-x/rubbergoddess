6d054a1ad15db8516d41be275467a4e73803f699
sinus-x <14353790+sinus-x@users.noreply.github.com>
2020-06-26 21:43:57 +0200

    Judge: Scrolling

--- cogs/judge.py
+++ cogs/judge.py
@@ -32,32 +32,35 @@ class Judge(rubbercog.Rubbercog):
         if db_subject is None:
             return await ctx.send("that is not valid subject")
 
+        title = "Review: " + subject
         name = db_subject.name if db_subject.name is not None else discord.Embed.Empty
         if name is not discord.Embed.Empty and db_subject.category is not None:
             name += f" ({db_subject.category})"
 
         db_reviews = repo_r.get_subject_reviews(subject)
         if db_reviews.count() == 0:
-            embed = self.embed(ctx=ctx, description=name)
+            embed = self.embed(ctx=ctx, title=title, description=name)
             embed.add_field(name="no reviews", value="no one has reviewed this subject yet")
         else:
+            _total = 0
+            for db_review in db_reviews:
+                _total += db_review.Review.tier
+            average = _total / db_reviews.count()
+
             review = db_reviews[0].Review
-            embed = self.embed(ctx=ctx, description=name, page=(1, db_reviews.count()))
-            # TODO Compute average mark
-            embed.add_field(name="average mark", value="-1", inline=False)
-            # TODO Check if user is anonymous
-            embed.add_field(name="Author", value=self.bot.get_user(int(review.discord_id)))
-            # TODO Translate
-            embed.add_field(name="Mark", value=review.tier)
-            # TODO Translate
-            embed.add_field(name="Date", value=review.date)
+            embed = self.embed(ctx=ctx, title=title, description=name, page=(1, db_reviews.count()))
+            embed = self.fill_subject_embed(embed, review, average)
 
-            embed.add_field(name="Text", value=review.text_review, inline=False)
+        message = await ctx.send(embed=embed)
+        if db_reviews.count() > 1:
+            await message.add_reaction("◀")
+            await message.add_reaction("▶")
+        if db_reviews.count() > 0:
+            await message.add_reaction("👍")
+            await message.add_reaction("🛑")
+            await message.add_reaction("👎")
 
-            embed.add_field(name="👍", value=f"{repo_r.get_votes_count(review.id, True)}")
-            embed.add_field(name="👎", value=f"{repo_r.get_votes_count(review.id, False)}")
-
-        await ctx.send(embed=embed)
+        await utils.delete(ctx)
 
     @review.command(name="add", aliases=["update"])
     async def review_add(self, ctx, subject: str, mark: int, *, text: str):
@@ -118,14 +121,122 @@ class Judge(rubbercog.Rubbercog):
     ##
     ## Listeners
     ##
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction: discord.Reaction, user: discord.User):
+        # check for wanted embed
+        if (
+            user.bot
+            or len(reaction.message.embeds) != 1
+            or not reaction.message.embeds[0].title.startswith("Review: ")
+        ):
+            return
 
-    ##
-    ## Logic
-    ##
+        scroll = False
+        vote = False
+        scroll_delta = 0
+        vote_value = 0
+
+        # check for scroll availability
+        if hasattr(reaction, "emoji"):
+            # scrolling
+            if str(reaction.emoji) == "◀":
+                scroll = True
+                scroll_delta = -1
+            elif str(reaction.emoji) == "▶":
+                scroll = True
+                scroll_delta = 1
+            # voting
+            elif str(reaction.emoji) == "👍":
+                vote = True
+                vote_value = 1
+            elif str(reaction.emoji) == "🛑":
+                vote = True
+                vote_value = 0
+            elif str(reaction.emoji) == "👎":
+                vote = True
+                vote_value = -1
+            # invalid
+            else:
+                # invalid reaction
+                return await self._remove_reaction(reaction, user)
+        else:
+            # invalid reaction
+            return await self._remove_reaction(reaction, user)
+
+        embed = reaction.message.embeds[0]
+        if embed.footer == discord.Embed.Empty or " | " not in embed.footer.text:
+            return await self._remove_reaction(reaction, user)
+
+        # get reviews for given subject
+        subject = embed.title.replace("Review: ", "")
+        reviews = repo_r.get_subject_reviews(subject)
+
+        _total = 0
+        for review in reviews:
+            _total += review.Review.tier
+        average = _total / reviews.count()
+
+        # get page
+        footer_text = embed.footer.text
+        pages = footer_text.split(" | ")[-1]
+        page_current = int(pages.split("/")[0]) - 1
+
+        page = (page_current + scroll_delta) % reviews.count()
+        footer_text = footer_text.replace(pages, f"{page+1}/{reviews.count()}")
+
+        # get new review
+        review = reviews[page].Review
+
+        if vote:
+            # apply vote
+            if str(user.id) == review.discord_id:
+                return await self._remove_reaction(reaction, user)
+            if vote_value == 0:
+                repo_r.remove_vote(review.id, str(user.id))
+            else:
+                repo_r.add_vote(review.id, vote_value == 1, str(user.id))
+
+        # update embed
+        embed = self.fill_subject_embed(embed, review, average)
+        embed.set_footer(text=footer_text, icon_url=embed.footer.icon_url)
+        await reaction.message.edit(embed=embed)
+
+        await self._remove_reaction(reaction, user)
 
     ##
     ## Helper functions
     ##
+    async def _remove_reaction(self, reaction, user):
+        try:
+            await reaction.remove(user)
+        except:
+            pass
+
+    ##
+    ## Logic
+    ##
+    def fill_subject_embed(
+        self, embed: discord.Embed, review: object, average: float
+    ) -> discord.Embed:
+        # reset any previous
+        embed.clear_fields()
+
+        # add content
+        embed.add_field(name=f"**ID**: {review.id}", value=f"⌀: {average:.1f}", inline=False)
+
+        # TODO Translate
+        name = self.bot.get_user(int(review.discord_id)) or "user left"
+        if review.anonym:
+            name = "..."
+        embed.add_field(name=name, value=review.date)
+        embed.add_field(name="Mark", value=review.tier)
+
+        embed.add_field(name="Text", value=review.text_review, inline=False)
+
+        embed.add_field(name="👍", value=f"{repo_r.get_votes_count(review.id, True)}")
+        embed.add_field(name="👎", value=f"{repo_r.get_votes_count(review.id, False)}")
+
+        return embed
 
     ##
     ## Error handlers
