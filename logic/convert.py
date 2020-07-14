5f58a7210c42280bd3f3f5e187ab64a76d0d16a2
sinus-x <14353790+sinus-x@users.noreply.github.com>
2020-07-14 23:05:46 +0200

    Delete convert.py and its emote_number_from_int()
    We'll probably use it sometime in the future. It is currently not used

--- logic/convert.py
+++
@@ -1,8 +0,0 @@
-class Convert:
-    @staticmethod
-    def emote_number_from_int(number):
-        """Convert number to emote"""
-        numbers = ("0️⃣", "1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣")
-        if number > len(numbers):
-            return None
-        return numbers[number]
