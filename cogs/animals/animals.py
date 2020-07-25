ef780363d57b4f08ec301735ed7533d08aa731a4
sinus-x <14353790+sinus-x@users.noreply.github.com>
2020-07-25 14:51:17 +0200

    Animals in a separate directory

--- cogs/animals/animals.py
+++ cogs/animals/animals.py
@@ -1,9 +1,8 @@
 import discord
 from discord.ext import commands
 
+from cogs.resource import CogConfig, CogText
 from core import check, rubbercog, utils
-from core.config import config
-from core.text import text
 
 
 class Animals(rubbercog.Rubbercog):
@@ -11,17 +10,20 @@ class Animals(rubbercog.Rubbercog):
 
     def __init__(self, bot):
         super().__init__(bot)
+        self.config = CogConfig("animals")
+        self.text = CogText("animals")
+
         self.channel = None
         self.role = None
 
     def getChannel(self):
         if self.channel is None:
-            self.channel = self.bot.get_channel(config.get("animals", "channel"))
+            self.channel = self.bot.get_channel(self.config.get("channel"))
         return self.channel
 
     def getRole(self):
         if self.role is None:
-            self.role = self.getChannel().guild.get_role(config.get("animals", "role"))
+            self.role = self.getChannel().guild.get_role(self.config.get("role"))
         return self.role
 
     ##
@@ -53,10 +55,11 @@ class Animals(rubbercog.Rubbercog):
         if reaction.message.channel != self.getChannel():
             return
 
-        if len(reaction.message.embeds) != 1 or reaction.message.embeds[0].title != text.get(
-            "animals", "title"
-        ):
+        # fmt: off
+        if len(reaction.message.embeds) != 1 \
+        or reaction.message.embeds[0].title != self.text.get("title"):
             return
+        # fmt: on
 
         if str(reaction.emoji) not in ("☑️", "❎"):
             return await utils.remove_reaction(reaction, user)
@@ -65,15 +68,16 @@ class Animals(rubbercog.Rubbercog):
         member = reaction.message.guild.get_member(member_id)
 
         for r in reaction.message.reactions:
-            if r.emoji == "☑️" and r.count > config.get("animals", "limit"):
+            if r.emoji == "☑️" and r.count > self.config.get("limit"):
                 await member.add_roles(self.getRole())
                 await self.event.user(member, "New animal!")
-                await self.getChannel().send(text.fill("animals", "join", mention=member.mention))
+                await self.getChannel().send(self.text.get("join", mention=member.mention))
                 break
-            elif r.emoji == "❎" and r.count > config.get("animals", "limit"):
+            elif r.emoji == "❎" and r.count > self.config.get("limit"):
                 await member.remove_roles(self.getRole())
+                # TODO Log event only if user had `animal` role
                 await self.event.user(member, "Animal left.")
-                await self.getChannel().send(text.fill("animals", "leave", mention=member.mention))
+                await self.getChannel().send(self.text.get("leave", mention=member.mention))
                 break
         else:
             return
@@ -85,12 +89,9 @@ class Animals(rubbercog.Rubbercog):
 
     async def check(self, member: discord.Member):
         # create embed
-        embed = self.embed(
-            title=text.get("animals", "title"), description=f"{str(member)} | {member.id}"
-        )
+        embed = self.embed(title=self.text.get("title"), description=f"{str(member)} | {member.id}")
         embed.add_field(
-            name="\u200b",
-            value=text.fill("animals", "required", limit=config.get("animals", "limit")),
+            name="\u200b", value=self.text.get("required", limit=self.config.get("limit"))
         )
         embed.set_image(url=member.avatar_url)
         message = await self.getChannel().send(embed=embed)
@@ -101,7 +102,3 @@ class Animals(rubbercog.Rubbercog):
         except Exception as e:
             await self.event.user(member, "Could not pin Animal check embed.")
             await self.console.warning("animals", "Could not unpin embed", e)
-
-
-def setup(bot):
-    bot.add_cog(Animals(bot))
