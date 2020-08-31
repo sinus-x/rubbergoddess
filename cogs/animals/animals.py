7b0430f74af827e38a18c2a400128a75c05f6299
sinus-x <14353790+sinus-x@users.noreply.github.com>
2020-08-31 19:51:49 +0200

    Animals: "Not an animal" logs are more descriptive

--- cogs/animals/animals.py
+++ cogs/animals/animals.py
@@ -52,7 +52,7 @@ class Animals(rubbercog.Rubbercog):
 
         # only act if their avatar is not default
         if member.avatar_url == member.default_avatar_url:
-            await self.event.user(str(member), "Not an animal (default avatar).")
+            await self.event.user(f"{member} joined.", "Not an animal (default avatar).")
             return
 
         await self.check(member, "on_member_join")
@@ -70,7 +70,7 @@ class Animals(rubbercog.Rubbercog):
 
         # only act if user has changed their avatar
         if before.avatar_url == after.avatar_url:
-            await self.event.user(str(after), "Not an animal (default avatar).")
+            await self.event.user(f"{after} updated.", "Not an animal (default avatar).")
             return
 
         await self.check(after, "on_user_update")
@@ -85,7 +85,7 @@ class Animals(rubbercog.Rubbercog):
 
         # only act if their avatar is not default
         if after.avatar_url == after.default_avatar_url:
-            await self.event.user(str(after), "Not an animal (default avatar).")
+            await self.event.user(f"{after} verified.", "Not an animal (default avatar).")
             return
 
         await self.check(after, "on_member_update")
