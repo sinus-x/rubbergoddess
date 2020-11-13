145353c939d8fea43caced7e07a16f3615dc142b
sinus-x <14353790+sinus-x@users.noreply.github.com>
2020-11-13 13:30:19 +0100

    Fix room_check firing on non-main guild

--- core/utils.py
+++ core/utils.py
@@ -86,6 +86,10 @@ async def room_check(ctx: commands.Context):
     if not isinstance(ctx.channel, discord.TextChannel):
         return
 
+    # Do not send message if the message has been sent to other guild
+    if ctx.guild.id != config.guild_id:
+        return
+
     if ctx.channel.id not in config.get("channels", "bot allowed"):
         # we do not have `bot` variable, so we have to construct the botroom mention directly
         await ctx.send(
