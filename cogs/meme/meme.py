b4bddfacec9fa48c5b21c4f3603fc9e26615c001
sinus-x <14353790+sinus-x@users.noreply.github.com>
2021-07-01 13:10:41 +0200

    Meme: Include user avatar in the embed
    Resolves #282

--- cogs/meme/meme.py
+++ cogs/meme/meme.py
@@ -304,6 +304,7 @@ class Meme(rubbercog.Rubbercog):
             description=f"**{self.sanitise(user.display_name)}**\n"
             f"{self.text.get('relations_help')}",
         )
+        embed.set_thumbnail(url=user.avatar_url_as(size=256))
 
         for action in ("hug", "pet", "hyperpet", "slap", "spank", "whip", "bonk"):
             lookup = repo_i.get_user_action(user.id, ctx.guild.id, action)
