3e4d2e22108623576c04574ddadc2bc148062ef0
sinus-x <14353790+sinus-x@users.noreply.github.com>
2021-08-29 11:16:49 +0200

    Backup: Make the script importable

--- cogs/backup/write.py
+++ cogs/backup/write.py
@@ -5,49 +5,6 @@ import sys
 import sqlalchemy
 
 
-parser = argparse.ArgumentParser(
-    description="Data converter for Rubbergoddess dumps",
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    allow_abbrev=False,
-)
-parser.add_argument(
-    "--input",
-    required=True,
-    help="Input file path",
-)
-parser.add_argument(
-    "--output",
-    required=True,
-    help="Output file path",
-)
-parser.add_argument(
-    "--fast",
-    action="store_true",
-    help="Do not write emojis or avatars",
-)
-parser.add_argument(
-    "--format",
-    # TODO Add "guess from extension" option and make it default
-    choices=("html", "latex", "json", "csv"),
-    default="html",
-    help="Output format",
-)
-
-args = parser.parse_args()
-if args.fast != True:
-    print("Only fast formatting is supported.")
-    sys.exit(1)
-if args.format not in ("html", "csv"):
-    print('The only supported format is "html" and "csv".')
-    sys.exit(1)
-
-try:
-    engine = sqlalchemy.create_engine("sqlite:///" + args.input)
-except sqlalchemy.exc.SQLAlchemyError as exc:
-    print(str(exc))
-    sys.exit(1)
-
-
 def get_datetime(id: int) -> datetime.datetime:
     return datetime.datetime.fromtimestamp(((id >> 22) + 1420070400000) / 1000)
 
@@ -55,6 +12,7 @@ def get_datetime(id: int) -> datetime.datetime:
 class Formatter:
     def __init__(self, users: list):
         self.users = users
+        self.title = "Rubbergoddess dump"
 
     def get_user(self, user_id: int) -> object:
         for user in self.users:
@@ -78,7 +36,7 @@ class HTMLFormatter(Formatter):
             "<html>",
             "<head>",
             "<meta charset='utf-8'>",
-            "<title>Rubbergoddess dump</title>",
+            f"<title>{self.title}</title>",
             "<style>",
             "body {margin: 0;padding: 0;background: #36393F;color: #DCDDDE;font-family: Whitney,Helvetica Neue,Helvetica,Arial,sans-serif;}",
             ".message {padding: .3em .5em;}",
@@ -151,26 +109,59 @@ class CSVFormatter(Formatter):
         return ""
 
 
-connection = engine.connect()
-metadata = sqlalchemy.MetaData()
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description="Data converter for Rubbergoddess dumps",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        allow_abbrev=False,
+    )
+    parser.add_argument(
+        "--input",
+        required=True,
+        help="Input file path",
+    )
+    parser.add_argument(
+        "--output",
+        required=True,
+        help="Output file path",
+    )
+    parser.add_argument(
+        "--fast",
+        action="store_true",
+        help="Do not write emojis or avatars",
+    )
+    parser.add_argument(
+        "--format",
+        # TODO Add "guess from extension" option and make it default
+        choices=("html", "latex", "json", "csv"),
+        default="html",
+        help="Output format",
+    )
+    return parser
 
 
-def get_table(name: str, *, reverse: bool = False):
-    table = sqlalchemy.Table(name, metadata, autoload=True, autoload_with=engine)
-    if reverse:
-        # reverse the order
-        query = sqlalchemy.select([table]).order_by(table.c.id.asc())
-    else:
-        query = sqlalchemy.select([table])
-    proxy = connection.execute(query)
-    return proxy.fetchall()
+class Database:
+    def __init__(self, dbfile: str):
+        try:
+            self.engine = sqlalchemy.create_engine("sqlite:///" + dbfile)
+        except sqlalchemy.exc.SQLAlchemyError as exc:
+            print(str(exc), file=sys.stderr)
+            sys.exit(1)
+
+        self.connection = self.engine.connect()
+        self.metadata = sqlalchemy.MetaData()
+
+    def get_table(self, name: str, *, reverse: bool = False):
+        table = sqlalchemy.Table(name, self.metadata, autoload=True, autoload_with=self.engine)
+        if reverse:
+            query = sqlalchemy.select([table]).order_by(table.c.id.asc())
+        else:
+            query = sqlalchemy.select([table])
+        proxy = self.connection.execute(query)
+        return proxy.fetchall()
 
 
-users = get_table("users")
-messages = get_table("messages", reverse=True)  # [:100]
-
-
-def get_formatter(formatter: str):
+def get_formatter(formatter: str, users):
     if formatter == "csv":
         return CSVFormatter(users)
     if formatter == "html":
@@ -178,11 +169,37 @@ def get_formatter(formatter: str):
     return ValueError("Formatter not supported.")
 
 
-formatter = get_formatter(args.format)
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+    if args.fast != True:
+        print("Only fast formatting is supported.")
+        sys.exit(1)
+    if args.format not in ("html", "csv"):
+        print('The only supported format is "html" and "csv".')
+        sys.exit(1)
+
+    print("> Loading database...")
+    database = Database(args.input)
+
+    print("> Loading users...")
+    users = database.get_table("users")
+
+    print("> Loading messages...")
+    messages = database.get_table("messages", reverse=True)
+
+    print("> Loading formatter...")
+    formatter = get_formatter(args.format, users)
+
+    print("> Writing data...")
+    with open(args.output, "w") as handle:
+        handle.write(formatter.header())
+        for message in messages:
+            handle.write(formatter.format_message(message))
+        handle.write(formatter.footer())
+
+    print("> Done")
 
 
-with open(args.output, "w") as handle:
-    handle.write(formatter.header())
-    for message in messages:
-        handle.write(formatter.format_message(message))
-    handle.write(formatter.footer())
+if __name__ == "__main__":
+    main()
